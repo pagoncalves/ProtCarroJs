b4c3c1bebe6ef559f97279b3d00f57e226a36f11
Patricia Goncalves <patriciagpereira89@gmail.com>
2021-02-06 11:11:14 -0300

    inicio

---
+++ protcarro.js
@@ -0,0 +1,37 @@
+var btlig = window.document.getElementById('ligar')
+//var velocidade = window.document.getElementById('vel')
+var resVel = window.document.getElementById('vel')
+function ligar() {
+
+    btlig.style.backgroundColor = 'green'
+    btlig.innerText = "On"
+}
+
+function desligar() {
+    btlig.style.backgroundColor = 'red'
+    btlig.innerText = "Off"
+}
+
+function acelerar() {
+     var maisVel = 1;
+    resVel.innerHTML = Number(maisVel.value) + 1;
+       
+              
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
