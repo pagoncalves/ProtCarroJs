c2c4f935a4df2ef1bdbd5a657b02eb2bdc9080fc
Patricia Goncalves <patriciagpereira89@gmail.com>
2021-02-27 09:57:18 -0300

    Incluso Lib. Jquery

--- protcarro.js
+++ protcarro.js
@@ -1,31 +1,24 @@
-var btlig = window.document.getElementById('ligar')
-//var velocidade = window.document.getElementById('vel')	
-var resVel = window.document.getElementById('vel')
 var ligado = false;
 var velocidade = Number(0);
 var direcao = Number(0);
-var imagem = window.document.getElementById('imgVol')
-var atua = window.document.getElementById("odo")
 var m = Number(0)
-
-
-var m = Number(1);
+var timer = setInterval(atualizaOdom,1000) 
 
 function chave() {
     if (ligado == true) {
-        btlig.style.backgroundColor = 'rgb(133, 18, 18)';
-        btlig.innerText = "Off";
+        $("#ligar").css("backgroundColor",'rgb(133, 18, 18)');
+        $("#ligar").html("Off");
         velocidade = 0;
-        resVel.innerHTML = 0;
+        $("#vel").html(0);
         ligado = false;
         direcao = 0;
-        imagem.src = "V_0.png" 
-        atua.innerHTML = 0;
+        $("#imgVol").attr('src',"V_0.png")
+        m = 0;
         
 
     } else {
-        btlig.style.backgroundColor = 'green'
-        btlig.innerText = "On"
+        $("#ligar").css("backgroundColor", 'green');
+        $("#ligar").html("On");
         ligado = true
         
 
@@ -36,130 +29,84 @@ function chave() {
 function acelerar() {
     if (ligado == true && velocidade < 45) {
         velocidade = velocidade + 1;
-        resVel.innerHTML = `${velocidade} m/s`;
-        m = (m + (velocidade * 1))/100 ;
-        atua.innerHTML =  ` ${m.toFixed(2)}m/s` ;
-        
+        $("#vel").html(`${velocidade} m/s`);
+                
     }
 }
 
 function frear() {
     if (ligado == true && velocidade > 0) {
         velocidade = velocidade - 1;
-        resVel.innerHTML = `${velocidade} m/s`;
+        $("#vel").html(`${velocidade} m/s`);
+        
     }
 }
 
 function esquerda() {
     //vai do centro para esquerda
-    if (ligado == true && direcao > -90 && direcao <= 0) {
+    if (ligado == true && direcao > -90) {
         direcao = direcao - 10;
 
-        if (direcao == 0) {
-            imagem.src = "V_0.png";
-        } else if (direcao == -10) {
-            imagem.src = "V_10.png";
-        } else if (direcao == -20) {
-            imagem.src = "V_20.png";
-        } else if (direcao == -30) {
-            imagem.src = "V_30.png";
-        } else if (direcao == -40) {
-            imagem.src = "V_40.png";
-        } else if (direcao == -50) {
-            imagem.src = "V_50.png";
-        } else if (direcao == -60) {
-            imagem.src = "V_60.png";
-        } else if (direcao == -70) {
-            imagem.src = "V_70.png";
-        } else if (direcao == -80) {
-            imagem.src = "V_80.png";
-        } else {
-            imagem.src = "V_90.png";
-        }
+      desenhavolante();
      
     }
-    if (ligado == true && direcao > 0 && direcao <= 90) {
-        direcao = direcao - 10;
-
-    if (direcao == 0) {
-        imagem.src = "V_0.png";
-    } else if (direcao == 10) {
-        imagem.src = "V_10d.png";
-    } else if (direcao == 20) {
-        imagem.src = "V_20d.png";
-    } else if (direcao == 30) {
-        imagem.src = "V_30d.png";
-    } else if (direcao == 40) {
-        imagem.src = "V_40d.png";
-    } else if (direcao == 50) {
-        imagem.src = "V_50d.png";
-    } else if (direcao == 60) {
-        imagem.src = "V_60d.png";
-    } else if (direcao == 70) {
-        imagem.src = "V_70d.png";
-    } else if (direcao == 80) {
-        imagem.src = "V_80d.png";
-    }
-   
-
-            }
+    
 }
 
 function direita() {
     //esta na esquerda e vai pra direita
-    if (ligado == true && direcao <= 0 && direcao < 90) {
+    if (ligado == true && direcao < 90) {
         direcao = direcao + 10;
 
-        if (direcao == 0) {
-            imagem.src = "V_0.png";
-        } else if (direcao == -10) {
-            imagem.src = "V_10.png";
-        } else if (direcao == -20) {
-            imagem.src = "V_20.png";
-        } else if (direcao == -30) {
-            imagem.src = "V_30.png";
-        } else if (direcao == -40) {
-            imagem.src = "V_40.png";
-        } else if (direcao == -50) {
-            imagem.src = "V_50.png";
-        } else if (direcao == -60) {
-            imagem.src = "V_60.png";
-        } else if (direcao == -70) {
-            imagem.src = "V_70.png";
-        } else if (direcao == -80) {
-            imagem.src = "V_80.png";
-        }
-      
-    }
-    //vai do centro para direita
-   if (ligado == true && direcao > 0 && direcao < 90 ) {
-        direcao = direcao + 10;
-
-
-       if (direcao == 0) {
-            imagem.src = "V_0.png";
-        } else if (direcao == 10) {
-            imagem.src = "V_10d.png";
-        } else if (direcao == 20) {
-            imagem.src = "V_20d.png";
-        } else if (direcao == 30) {
-            imagem.src = "V_30d.png";
-        } else if (direcao == 40) {
-            imagem.src = "V_40d.png";
-        } else if (direcao == 50) {
-            imagem.src = "V_50d.png";
-        } else if (direcao == 60) {
-            imagem.src = "V_60d.png";
-        } else if (direcao == 70) {
-            imagem.src = "V_70d.png";
-        } else if (direcao == 80) {
-            imagem.src = "V_80d.png";
-        } else {
-            imagem.src = "V_90d.png";
-                  }
-    }
+        desenhavolante();
+    }   
 }
 
-function desenhacarro(){
-    
+function desenhavolante(){
+  
+    if (direcao == 0) {
+        $("#imgVol").attr('src',"V_0.png");
+    } else if (direcao == -10) {
+        $("#imgVol").attr('src',"V_10.png");
+    } else if (direcao == -20) {
+        $("#imgVol").attr('src',"V_20.png");
+    } else if (direcao == -30) {
+        $("#imgVol").attr('src',"V_30.png");
+    } else if (direcao == -40) {
+        $("#imgVol").attr('src',"V_40.png");
+    } else if (direcao == -50) {
+        $("#imgVol").attr('src',"V_50.png");
+    } else if (direcao == -60) {
+        $("#imgVol").attr('src',"V_60.png");
+    } else if (direcao == -70) {
+        $("#imgVol").attr('src',"V_70.png");
+    } else if (direcao == -80) {
+        $("#imgVol").attr('src',"V_80.png");
+    } else if  (direcao == -90 ){
+        $("#imgVol").attr('src',"V_90.png");
+    } else if (direcao == 10) {
+        $("#imgVol").attr('src',"V_10d.png");
+    } else if (direcao == 20) {
+        $("#imgVol").attr('src',"V_20d.png");
+    } else if (direcao == 30) {
+        $("#imgVol").attr('src',"V_30d.png");
+    } else if (direcao == 40) {
+        $("#imgVol").attr('src',"V_40d.png");
+    } else if (direcao == 50) {
+        $("#imgVol").attr('src',"V_50d.png");
+    } else if (direcao == 60) {
+        $("#imgVol").attr('src',"V_60d.png");
+    } else if (direcao == 70) {
+        $("#imgVol").attr('src',"V_70d.png");
+    } else if (direcao == 80) {
+        $("#imgVol").attr('src',"V_80d.png");
+    } else if (direcao == 90){
+        $("#imgVol").attr('src',"V_90d.png");
+              }
+
+}
+
+function atualizaOdom(){
+    m = m + (velocidade * 1)/100 ;
+     $("#odo").html(` ${m.toFixed(2)} m`)
 }
